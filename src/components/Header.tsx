254f6cdf422edff11c3a1e23e02d1191c640e223
advaith.r <advaith.r@flytxt.com>
2025-06-22 18:40:44 +0530

    changed btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,8 +121,7 @@ const Header = () => {
             ))}
             <button
               onClick={() => window.open('/resume.pdf', '_blank')}
-              className="block mt-4 px-4 py-2 border border-green-500 text-green-500 hover:bg-green-500 hover:text-white transition-all duration-300 rounded-lg text-center"
-            >
+              className="block py-2 text-green-500 transition-colors duration-300"            >
               Resume
             </button>
           </motion.div>
